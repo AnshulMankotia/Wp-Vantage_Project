596643ec0832db76953bad1591be254054796be0
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-05-08 11:55:48 +0530

    initialize

---
+++ main.js
@@ -0,0 +1,21 @@
+const navIcon=document.querySelector('.navIcon');
+const mobileMenu=document.querySelector('.mobile-menu')
+navIcon.addEventListener('click',() => {
+    navIcon.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+})
+
+const accordion = document.getElementsByClassName('tab');
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function() {
+    // Remove "active" class from all tabs
+    for (let j = 0; j < accordion.length; j++) {
+      if (j !== i) {
+        accordion[j].classList.remove('active');
+      }
+    }
+    // Toggle "active" class on the clicked tab
+    this.classList.toggle('active');
+  });
+}
